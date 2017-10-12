ef0be46b882594e3928b6299b4bd4e37bfd8baf7
scharles1 <sethec@stanford.edu>
2017-10-12 09:53:21 -0700

    another commit

--- project1.jl
+++ project1.jl
@@ -1,12 +1,13 @@
 using LightGraphs
 using DataFrames
 
-"""
-    write_gph(dag::DiGraph, idx2names, filename)
-
-Takes a DiGraph, a Dict of index to names and a output filename to write the graph in `gph` format.
-"""
-function write_gph(dag::DiGraph, idx2names, filename)
+############################################################
+# Function: write_gph(dag::DiGraph, idx2names, filename)
+#
+# Description: Takes a DiGraph, a Dict of index to names 
+# and a output filename to write the graph in `gph` format.
+############################################################
+function write_gph(dag::DiGraph, idx2names::Dict, filename::String)
     open(filename, "w") do io
         for edge in edges(dag)
             @printf(io, "%s, %s\n", idx2names[src(edge)], idx2names[dst(edge)])
@@ -14,25 +15,47 @@ function write_gph(dag::DiGraph, idx2names, filename)
     end
 end
 
-function compute(infile, outfile)
+############################################################
+# Function: compute(data)
+#
+# Description:
+############################################################
+function compute(infile::String, outfile::String)
     data = readtable(infile)
-    idx2names = get_dict(data)
+    i2names = get_dict(data)
+    graphs2search = possibleGraphs(size(data,2))
     #
-    #@printf(io, infile)
 end
 
-function get_dic(data)
-    names = names(data)
-    l = length(names)
+############################################################
+# Function: get_dic(data)
+#
+# Description: 
+############################################################
+function get_dict(data::DataFrames.DataFrame)
+    Names = names(data)
+    l = length(Names)
     keys = 1:l
-    return Dict(keys[i]=>names[i] for i = 1:1:l)
+    return Dict(keys[i]=>Names[i] for i = 1:1:l)
 end
 
-if length(ARGS) != 2
-    error("usage: julia project1.jl <infile>.csv <outfile>.gph")
+############################################################
+# Function: possibleGraphs(data)
+#
+# Description: 
+############################################################
+function possibleGraphs(n::Integer)
+    graphs2Search = Set{DiGraph}()
+    baseGraph = DiGraph 
+    for i = 1:1:n
+        
+    end
 end
 
-inputfilename = ARGS[1]
-outputfilename = ARGS[2]
+#if length(ARGS) != 2
+#    error("usage: julia project1.jl <infile>.csv <outfile>.gph")
+#end
 
+inputfilename = "small.csv"#ARGS[1]
+outputfilename = "short.gph"#ARGS[2]
 compute(inputfilename, outputfilename)
