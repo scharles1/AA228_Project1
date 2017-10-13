29e379e2fee4384a87401d6c017349cfb3d1301c
scharles1 <sethec@stanford.edu>
2017-10-13 09:32:21 -0700

    new commit before changes

--- project1.jl
+++ project1.jl
@@ -23,8 +23,14 @@ end
 function compute(infile::String, outfile::String)
     data = readtable(infile)
     i2names = get_dict(data)
-    graphs2search = possibleGraphs(size(data,2))
-    #
+    graphs2search = possibleGraphs(10)
+    for g in graphs2search
+       for e in edges(g)
+           show(e)
+        end
+        @printf("\n\n")
+    end
+    show(length(graphs2search))
 end
 
 ############################################################
@@ -36,7 +42,7 @@ function get_dict(data::DataFrames.DataFrame)
     Names = names(data)
     l = length(Names)
     keys = 1:l
-    return Dict(keys[i]=>Names[i] for i = 1:1:l)
+    return Dict(keys[i]=>Names[i] for i = 1:l)
 end
 
 ############################################################
@@ -44,12 +50,90 @@ end
 #
 # Description: 
 ############################################################
-function possibleGraphs(n::Integer)
-    graphs2Search = Set{DiGraph}()
-    baseGraph = DiGraph 
-    for i = 1:1:n
-        
+function possibleGraphs(nVertices::Integer)
+    graphs2Search = Vector{DiGraph}()
+    queue = Vector{DiGraph}()
+    unshift!(graphs2Search, DiGraph(nVertices))
+    unshift!(queue, DiGraph(nVertices))
+    
+    while(!isempty(queue))
+        curr = pop!(queue)
+        if (!hasMarkovEquivalent(curr, graphs2Search))
+            unshift!(graphs2Search, copy(curr))
+        end
+        while(addRandEdge(curr))
+           unshift!(queue,copy(curr)) 
+        end
     end
+    return graphs2Search
+end
+        
+############################################################
+# Function: addRandEdge(g::DiGraph)
+#
+# Description: "Randomly" adds a new directed edge to the
+# the graph. Returns true if it was able to. Otherwise,
+# it returns false.
+############################################################
+function addRandEdge(g::DiGraph)
+    for i = 1:nv(g)
+        for j = 1:nv(g)
+            if i == j
+                continue
+            elseif has_edge(g, Edge(j,i))
+                continue
+            elseif add_edge!(g,i,j)
+                if has_self_loops(g)
+                    rem_edge!(g,i,j)
+                else
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
+############################################################
+# Function: hasMarkovEquivalent(g1::DiGraph, v::Vector{Digraph}
+#
+# Description: 
+############################################################
+function hasMarkovEquivalent(g1::DiGraph, v::Vector{DiGraph})
+    for g2 in v
+        if (!markovEquivalent(g1, g2))
+            continue
+        else
+            return true
+        end
+    end
+    return false
+end
+
+############################################################
+# Function: markovEquivalent(g1::DiGraph, g2::Digraph)
+#
+# Description: 
+############################################################
+function markovEquivalent(g1::DiGraph, g2::DiGraph)
+    if ne(g1) != ne(g2)
+        return false
+    end
+    for e1 = edges(g1)
+       if !has_edge(g2,e1) && !has_edge(g2, reverse(e1))
+           return false
+       end
+       for e2 = edges(g1)
+            if e1 == e2
+                continue
+            elseif dst(e1) == dst(e2)
+                if !has_edge(g2,e1) || !has_edge(g2,e2)
+                    return false
+                end
+            end
+       end
+    end
+    return true
 end
 
 #if length(ARGS) != 2
