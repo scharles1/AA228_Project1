0e666107be0fa55ce04b4a6ff8c2866796ef26dc
Seth Charles <scharles@Seths-MacBook-Air.local>
2017-10-15 17:56:57 -0700

    verified bayesian score with ian and tory

--- project1.jl
+++ project1.jl
@@ -1,7 +1,7 @@
 using LightGraphs
 using DataFrames
-using BayesNets
 using DataStructures
+using BayesNets
 
 ############################################################
 # Function: write_gph(dag::DiGraph, idx2names, filename)
@@ -26,13 +26,10 @@ function compute(infile::String, outfile::String)
     data = readtable(infile)
     i2names = getDict(data)
     g1 = DiGraph(8)
-    #g2 = runSethSearch(g1, data)
-    add_edge!(g1, 1, 5)
-    add_edge!(g1, 1, 7)
-    add_edge!(g1, 7, 5)
-    add_edge!(g1, 8, 5)
-    show(bayesianScore(g1, data))
-    #write_gph(g1, i2names, outfile)
+    g2 = runSethSearch(g1, data)
+    #runK2Search(g1, data)
+    show(bayesianScore(g2, data))
+    write_gph(g2, i2names, outfile)
 end
 
 ############################################################
@@ -50,12 +47,12 @@ end
 ############################################################
 # Function: k2Search(data)
 #
-# Description: 
+# Description: Implementing K2 search
 ############################################################
 function runK2Search(g::DiGraph, data::DataFrame)
-    bestScore = -100000000;
+    bestScore = -10000000000;
     n = nv(g)
-    for v = vertices(g)
+    for v = 1:n
         currScore = bayesianScore(g, data)
         prevScore = currScore - 1
         i = 0
@@ -85,45 +82,45 @@ end
 ############################################################
 # Function: runSethSearch(data)
 #
-# Description: 
+# Description: Uses a priority queue
 ############################################################
 function runSethSearch(g::DiGraph, data::DataFrame)
     bestScore = -100000000;
     pQ = PriorityQueue(Base.Order.Reverse)
-    enqueue!(pQ, copy(g), -1.0)
+    visitedGraphs = Vector{DiGraph}()
+    enqueue!(pQ, copy(g), 0.1)
     n = nv(g)
 
     while (!isempty(pQ))
         currG = dequeue!(pQ)
         node = rand(1:n)
         for v in vertices(g)
-            if(v == node)
-                continue
-            end
             e = Edge(node, v)
-            if has_edge(currG,e) || has_edge(currG,reverse(e))
+            if has_edge(currG,e) || has_edge(currG,reverse(e)) || (v == node)
                 continue
             end
             if !(add_edge!(currG, node, v))
                 continue
             end
-            if has_self_loops(currG)
+            if has_self_loops(currG) || hasMarkovEquivalent(currG, visitedGraphs)
                 rem_edge!(currG, node, v)
                 continue
             end
             currScore = bayesianScore(currG, data)
+            @printf("Current Score: %f\n",currScore)
             if(currScore > bestScore)
                 bestGraph = copy(currG)
                 bestScore = currScore
                 show(bestScore)
-                @printf("\n")
+                @printf("New Best Score: %d\n",bestScore)
                 for edge in edges(currG)
                     show(edge)
                     @printf("\n")
                 end
+                @printf("\n")
             end
             enqueue!(pQ, copy(currG), currScore)
-            printf("length of pq: %d\n", length(pq))
+            push!(visitedGraphs, copy(currG))
         end
     end
     return bestGraph
@@ -174,7 +171,8 @@ end
 ############################################################
 # Function: bayesianScore(g1::DiGraph, g2::Digraph)
 #
-# Description: 
+# Description: Utilizes the bayesianScoreCompenent and 
+#              the getM function
 ############################################################
 function bayesianScore(g::DiGraph, data::DataFrame) 
     #assume uniform prior with pseudocounts 1
@@ -187,6 +185,7 @@ function bayesianScore(g::DiGraph, data::DataFrame)
     return score
 end
 
+############################################################
 function bayesianScoreComponent(data::DataFrame, 
                                 node::Integer,
                                 parents::Vector{Integer},
@@ -203,8 +202,8 @@ function bayesianScoreComponent(data::DataFrame,
         end
     else
         for j = 1:nPInstances
-            subscore += lgamma(size(qTable)[1])
-            subscore -= lgamma(size(qTable)[1] + getM(data, node, j, 0, parents, qTable))
+            subscore += lgamma(nCInstances)
+            subscore -= lgamma(nCInstances + getM(data, node, j, 0, parents, qTable))
             for k = 1:nCInstances
                 subscore += lgamma(1 + getM(data, node, j, k, parents, qTable))
             end
@@ -213,6 +212,7 @@ function bayesianScoreComponent(data::DataFrame,
     return subscore
 end
 
+############################################################
 function getM(data::DataFrame,
               i::Integer,
               j::Integer, 
@@ -220,8 +220,8 @@ function getM(data::DataFrame,
               parents::Vector{Integer},
               qTable::Vector{Vector{Integer}})
     counts = 1:size(data)[1]
-    for i = 1:length(parents)
-        counts = intersect(counts, find(m->m == qTable[j][i], data[parents[i]]))
+    for n = 1:length(parents)
+        counts = intersect(counts, find(m->m == qTable[j][n], data[parents[n]]))
     end
     if k == 0
         return length(counts)
@@ -252,6 +252,7 @@ function getQTable(g::DiGraph, child::Integer, data::DataFrame)
     return table
 end
 
+############################################################
 function getQTable( table::Vector{Vector{Integer}},
                     pInstances::Vector{Vector{Integer}},
                     qi::Vector{Integer})
@@ -272,7 +273,7 @@ end
 ############################################################
 # Function: getParents(g::DiGraph, child::Integer)
 #
-# Description: 
+# Description: returns the parents of the child in g
 ############################################################
 function getParents(g::DiGraph, child::Integer)
     parents = Vector{Integer}()
